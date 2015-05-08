531d5be473dd1f515c9234f749b95db1c4b242ff
kogeo-note <kogeo.nekoyama@kiurisoft.com>
2015-05-08 16:31:48 +0900

    fix

--- crawler.rb
+++ crawler.rb
@@ -2,7 +2,6 @@ require "rubygems"
 require "watir-webdriver"
 require "nokogiri"
 require "uri"
-require "open-uri"
 require "csv"
 
 browser = Watir::Browser::new :firefox
@@ -29,19 +28,13 @@ CSV.read("keywords.txt").each do |keywords|
     end
     /([^\?]+)(?:\?.+)?/ =~ url
     url = $1
-    puts "  #{url}"
-    #begin
-    #  open(url)
-    #rescue OpenURI::HTTPError
-    #  next
-    #rescue URI::InvalidURIError
-    #end
+    puts "  Download : #{url}"
     /([^\/]+?)([\?#].*)?$/ =~ url
     filename = $1
     `wget -4 -O ./dl/#{keywords.join("_")}/#{filename} #{url} -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36" -nc --timeout=5 --tries=3 -q`
     if File.size?("./dl/#{keywords.join("_")}/#{filename}") == 0 then
       `rm "./dl/#{keywords.join("_")}/#{filename}"`
-      puts "    Download File is empty."
+      puts "    File is empty."
     end
     puts "    Finished."
   end
