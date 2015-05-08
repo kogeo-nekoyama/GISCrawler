641719ff4863db12e1c88550dde482224672564a
kogeo-note <kogeo.nekoyama@kiurisoft.com>
2015-05-08 16:29:31 +0900

    change and fix

--- crawler.rb
+++ crawler.rb
@@ -1,28 +1,48 @@
 require "rubygems"
 require "watir-webdriver"
 require "nokogiri"
-require "readline"
+require "uri"
+require "open-uri"
+require "csv"
 
-browser = Watir::Browser::new :ff
-while 1  do
-  inputs = []
-  puts "Input keywords.\n"
-  while 1 do
-    input = Readline.readline
-    if input == "q" then
-      break
-    end
-    inputs << input
-  end
-  `mkdir -p ./dl/#{inputs.join("_")}/`
-  browser.goto("https://www.google.co.jp/search?q=#{inputs.join('+')}&tbm=isch")
+browser = Watir::Browser::new :firefox
+
+CSV.read("keywords.txt").each do |keywords|
+  puts "Search '#{keywords.join(' ')}'"
+  `mkdir -p ./dl/#{keywords.join("_")}/`
+  browser.goto("https://www.google.co.jp/search?q=#{keywords.join('+')}&tbm=isch")
   html = Nokogiri::HTML(browser.html)
   html.xpath("//div[@class='rg_di rg_el']/a/@href").each do |img|
     /\?imgurl=(.+)&imgrefurl=/ =~ img.to_s
     url = $1
-    puts url
+    while 1 do
+      begin
+        if url == URI.unescape(url) then
+          break
+        else
+          url = URI.unescape(url)
+        end
+      rescue
+        url = $1
+        break
+      end
+    end
+    /([^\?]+)(?:\?.+)?/ =~ url
+    url = $1
+    puts "  #{url}"
+    #begin
+    #  open(url)
+    #rescue OpenURI::HTTPError
+    #  next
+    #rescue URI::InvalidURIError
+    #end
     /([^\/]+?)([\?#].*)?$/ =~ url
     filename = $1
-    `wget -O ./dl/#{inputs.join("_")}/#{filename} #{url} -q`
+    `wget -4 -O ./dl/#{keywords.join("_")}/#{filename} #{url} -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36" -nc --timeout=5 --tries=3 -q`
+    if File.size?("./dl/#{keywords.join("_")}/#{filename}") == 0 then
+      `rm "./dl/#{keywords.join("_")}/#{filename}"`
+      puts "    Download File is empty."
+    end
+    puts "    Finished."
   end
 end
